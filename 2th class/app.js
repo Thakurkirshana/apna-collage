eb8ddf6baca2c2bccef045d630778de8d6b0aea2
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-06-17 16:53:01 +0500

    out and in

---
+++ 2th class/app.js
@@ -0,0 +1,17 @@
+//Loops in js.  
+//   let num = prompt("enter the value")
+//   for( let i= 1; i<=7; i++){
+//     document.write ( num,"<br>")
+// }
+
+// for( let i=1; i<=5; i++){
+//     document.write("Namasty","<br>")
+// }
+
+//Calculate sum of 1 to 5
+let sum = 0;
+for (let i=1; i <= 6; i++){
+    sum = sum +i; //sum = 0 +1
+}
+document.write("sum = ",sum, "<br>"); 
+document.write("loop has ended"); 
