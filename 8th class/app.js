0d51921efc7fafd2e0f7745f86514a32e916c0e8
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-07-04 21:02:28 +0500

    just

---
+++ 8th class/app.js
@@ -0,0 +1,55 @@
+///   04/ 06/ 2024,
+// What is the console output for the following?//
+// let a = "Hello";
+// a = "world";
+// console.log(a);
+// let a = "world";
+// let b = `Hello ${a}!`;
+// console.log(b);
+// let a = "Hello";
+// a = prompt("world");
+// console.log(a);
+
+// let a = 5;
+// let b = 70;
+// let c = "5";
+// let d= b++;
+// console.log(d);
+
+// let result = 3 + 4 * 2 / 8; 
+// console.log(result)
+
+// let firstNum = 5;
+// let secondNum = 10;
+// firstNum++;
+// secondNum--;
+// let total = ++firstNum + secondNum;
+// console.log(firstNum,secondNum);
+// let total2 = 500 + 100 / 5 + total--;
+// console.log(total2);
+
+// const a = 5;
+// const b = 10;
+// console.log(a > 0 && b > 0); 
+// console.log(a == 5 && b == 4); 
+// console.log(true ||false); 
+// console.log(a == 3 || b == 10); 
+// console.log(a == 3 || b == 7);
+
+
+// let i =prompt("or kuch"); 
+// let j= prompt ("ornhi tum");
+// while(i<j){
+//     console.log(i)
+//     i++;
+// }
+// for (let i = 1; i <21; i++) {
+//     console.log(i);
+    
+// }let 
+// let a = 1;
+// let i =["or kya chal raya hai"]
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+    
+// }
