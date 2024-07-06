218a1a9462a271779228cdd7e05f6f08785cf93b
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-07-06 11:05:43 +0500

    function ki re

--- 9th class/index.js
+++ 9th class/index.js
@@ -59,18 +59,74 @@
 // });
 // console.log(output);
 
-let n = prompt(" Enter a number: ");
-let arr =[];
-for (let i=1; i<=n ; i++){
-    arr[i-1]=i;
-}
-console.log(arr);
- let  sum = arr.reduce((res,curr)=>{
-    return res+curr;
- });
- console.log("sum=",sum);
+// let n = prompt(" Enter a number: ");
+// let arr =[];
+// for (let i=1; i<=n ; i++){
+//     arr[i-1]=i;
+// }
+// console.log(arr);
+//  let  sum = arr.reduce((res,curr)=>{
+//     return res+curr;
+//  });
+//  console.log("sum=",sum);
 
- let  factorial = arr.reduce((res,curr)=>{
-    return res*curr;
- });
- console.log("factorial = ",factorial);
+//  let  factorial = arr.reduce((res,curr)=>{
+//     return res*curr;
+//  });
+//  console.log("factorial = ",factorial);
+
+// function name() {
+//    console.log("Hello World This is the Function of JS.");
+// }
+// name();
+// let name1=(a,b,c)=>{
+//    console.log(a+b+c)
+// }
+// name1(2,3,4);
+// let kuch=(a)=>{
+//    console.log(a);
+// }
+// kuch("or kya chal raya hai bhai")
+
+// let arr1=[1,2,3,4,5,6,7,8,9,0];
+// for (let i = 0; i < 100; i++) {
+//    console.log(arr1[i]); 
+
+// }
+// Local Function
+// function name(a,b) {
+//    console.log(a+b);
+// }name(2,3);
+// // Arrow Function..
+// let arrowFunc = (a, b) =>  a * b ;
+// console.log(arrowFunc (5,5));
+
+// let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//    newFunction=arr.map((X)=>{
+//      return x*2 ;
+//    })
+//    // console.log(newFunction,arr);
+// let newNumbers = arr.map((X) => {
+//     return X*2;
+// }
+// let arr =["ali",'waqar','wajid','reaju','sajad'];
+// let foo= prompt("enter the value")
+// let arr =[1,2,3,4,5,6,7,8,9,10]
+// arr.forEach((val)=>{
+//    console.log(val*foo)
+// })
+// let arr =[1,2,3,4,5];
+// arr.forEach((val)=>{
+//    console.log(val*2);
+// });
+
+// let arr = [1, 2,50, 3, 4, 5];
+// let newArrow = arr.filter((val)=>{
+//    return val % 2 !== 0;
+// })
+// console.log(newArrow)
+
+// let arr =[1,2,3,4,5,7,6,8,7,9,0];
+// let newArrow = arr.filter((val)=>{
+//    return val %2===0;
+// });console.log(newArrow);
