bd0a5e1b64e1a9343110e2336f563ade3b924d83
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-07-08 21:26:47 +0500

    this data type

---
+++ 10th class/app.js
@@ -0,0 +1,60 @@
+// alert("This is the Dom class page!")
+// DOM document object model(dom)
+//Dynamics changing Dom 
+// document.body.style.background="green";
+// document.body.style.background="pink"
+// document.body.childNodes.innerText="baba";
+// let heading = document.getElementById("");
+// console.log  (button);
+// let heading1= document.getElementsByClassName("heading");
+// console.log(heading1);
+// let para = document.getElementsByTagName("p");
+// console.log(para);
+
+// let firstelement = document.querySelector(".heading");
+// console.log(firstelement);
+// let allElement= document.querySelectorAll('.heading');
+// console.log(allElement);
+
+
+// let num = document.body.childNodes.innerText="king is the control of world.";
+// console.log(num);
+// let docu=document.getElementsByTagName("body");
+// console.log(docu);
+
+// console.dir(document.body);
+
+// let tagName = document.getElementsByTagName("td");
+// console.log(tagName);
+// let neWo = document.getElementById("bhI");
+// console.log(neWo.innerText);
+// let neQe = document.querySelectorAll("h1");
+// console.log(neQe);
+// let tagName1 = document.body.style.background="white";
+// document.body.style.color="black"
+// // console.log(tagName1);
+// let num =document.getElementsByClassName.innerText="apna collage";
+// console.log(num);
+// // document.body.childNodes.innerText="apna collage .com";
+// // document.body.childNodes.innerText="akjjiajdja"
+// let don=document.querySelectorAll("heading")
+// let apna=document.getElementById("apna");
+// console.log(apna);
+
+// let apka = document.getElementsByTagName("tr");
+// console.log(apka);
+
+// let button= document.querySelector(".button");
+// console.log(button.innerText);
+//  console.dir(button);
+// let div = document.querySelector("div");
+// console.log(div);
+// console.log(div.innerText);
+
+let heading = document.querySelector("h4");
+console.log(heading.innerText);
+let head = document.querySelector("h2");
+console.log(head.innerText);
+h2.innerText = h2.innerText + "From Apna Collage Student.";
+
+
