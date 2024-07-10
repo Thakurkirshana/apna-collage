4ed01ed81564f96f6e5c583f0db1d361b8665ff0
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-07-10 15:27:31 +0500

    just code

---
+++ 11th class/app.js
@@ -0,0 +1,109 @@
+// Chapter = 04; Logical Statements.
+// if and if else statements
+
+// let rain = true;
+// if (rain) {
+//     // code associated with the if block
+//     //will only be executed if the expression is true 
+//     console.log("Taking my Umbella when I need to go outside .");
+// } else {
+//     // code associated with the else block
+//     // we don't need an else block it is optipnal
+//     // this code will only be executed if the expression is false.
+//     console.log("I can leave my ubrella at home");
+// }
+// let age = 18
+// if (age > 18) {
+//     console.log("we've very sorry , but you can't get in under 18");
+// } else {
+//     console.log("welcome!");
+// }
+
+
+// let hobby = "coding";
+// if (hobby == "coding") {
+//     console.log("I love coding too!");
+// } else {
+//     console.log("Can you teach me that? ");
+// }
+// var num = 89
+// if (num < 16) {
+//     console.log("or kya chal raya hai bhai.");
+// }else{
+//     console.log(" kuch bhji nhi ch");
+// } IF 
+
+// let age = 10;
+// let cost = 0;
+// let message;
+// if (age < 3) {
+//  cost = 0;
+//  message = "Access is free under three.";
+// } else if (age >= 3 && age < 12) {
+//  cost = 5;
+//  message ="With the child discount, the fee is 5 dollars";
+//  }
+//  else if (age >= 12 && age < 65) {
+//   cost = 10;
+//   message ="A regular ticket costs 10 dollars.";
+//  } else {
+//   cost = 7;
+//   message ="A ticket is 7 dollars.";
+//  }
+//  console.log(message);
+//  console.log("Your Total cost "+cost);
+
+//  if(age < 1){
+//     console.log("Access is free under three.");
+//    } else if(age < 12) {
+//     console.log("With the child discount, the fee is 5 dollars");
+//    } else if(age < 65) {
+//     console.log("A regular ticket costs 10 dollars.");
+//    } else if(age >= 65) {
+//     console.log("A ticket is 7 dollars.");
+//    }
+//    let proMpt=prompt("User's Age");
+//    console.log(proMpt);
+// let age = 43;
+// if (age <20){
+//     console.log("Allow the Data Type of The Gega Type");
+// }else{
+// console.log("Array Logno tumara kya men janno mera Khuda jani.");
+// }
+// let access = age < 18 ? "denied" : "allowed";
+// let activity="activity"
+// if(activity === "Get up") {
+//     console.log("It is 6:30AM");
+//    } else if(activity === "Breakfast") {
+//     console.log("It is 7:00AM");
+//    } else if(activity === "Drive to work") {
+//     console.log("It is 8:00AM");
+//    } else if(activity === "Lunch") {
+//     console.log("It is 12.00PM");
+//    } else if(activity === "Drive home") {
+//     console.log("It is 5:00PM")
+//    } else if(activity === "Dinner") {
+//     console.log("It is 6:30PM");
+//    } //
+
+// switch (activity) {
+//     case "Get up":
+//         console.log("It is 6:30AM");
+//         break;
+//     case "Breakfast":
+//         console.log("It is 7:00AM");
+//         break;
+//     case "Drive to work":
+//         console.log("It is 8:00AM");
+//         break;
+//     case "Lunch":
+//         console.log("It is 12:00PM");
+//         break;
+//     case "Drive home":
+//         console.log("It is 5:00PM");
+//         break;
+//     case "Dinner":
+//         console.log("It is 6:30PM");
+//         break;
+// }
+
