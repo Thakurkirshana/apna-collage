dae58e9ef6d7d5a04e68d879fb604b89e055f484
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-07-13 22:13:59 +0500

    ok

--- 12th class/app.js
+++ 12th class/app.js
@@ -5,20 +5,21 @@ var btn = document.querySelector("button");
 var flag = 0
 
 btn.addEventListener("click", function () {
-    if(flag==0){
-        btn.innerText="OFF"
-        btn.style.backgroundColor="green"
- bulb.style.backgroundColor = "yellow"
- console.log("Clicked");
- flag=1;
-    }else{
-        btn.innerText="ON"
-    
-        btn.style.backgroundColor="red"
+    if (flag == 0) {
+        btn.innerText = "OFF"
+        btn.style.backgroundColor = "green"
+        bulb.style.backgroundColor = "yellow"
+        console.log("Clicked");
+        flag = 1;
+    } else {
+        btn.innerText = "ON"
+
+        btn.style.backgroundColor = "red"
         bulb.style.backgroundColor = "transparent"
- console.log("Click the Again");
- flag=0;
+        console.log("Click the Again");
+        flag = 0;
     }
-  
+
 })
+
 
