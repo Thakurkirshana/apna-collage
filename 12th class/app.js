b0ba5cf437e104a8634352a36ee10b24f7b66795
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-07-14 23:54:13 +0500

    this not easy

--- 12th class/app.js
+++ 12th class/app.js
@@ -1,25 +1,70 @@
-var a = document.querySelector("#bulb")
-var btn = document.querySelector("button");
+// var a = document.querySelector("#bulb")
+// var btn = document.querySelector("button");
 
 
-var flag = 0
+// var flag = 0
 
-btn.addEventListener("click", function () {
-    if (flag == 0) {
-        btn.innerText = "OFF"
-        btn.style.backgroundColor = "green"
-        bulb.style.backgroundColor = "yellow"
-        console.log("Clicked");
-        flag = 1;
+// btn.addEventListener("click", function () {
+//     if (flag == 0) {
+//         btn.innerText = "OFF"
+//         btn.style.backgroundColor = "green"
+//         bulb.style.backgroundColor = "yellow"
+//         console.log("Clicked");
+//         flag = 1;
+//     } else {
+//         btn.innerText = "ON"
+
+//         btn.style.backgroundColor = "red"
+//         bulb.style.backgroundColor = "transparent"
+//         console.log("Click the Again");
+//         flag = 0;
+//     }
+// })
+
+
+// var a = document.querySelector("#bulb");
+// var btn = document.querySelector("button");
+// var xyz = 0;
+// btn.addEventListener("click", function () {
+//     if (xyz == 0) {
+//         btn.innerHTML = "OFF"
+//         a.style.color = "blue"
+//         a.style.backgroundColor="black"
+//         console.log("clicked");
+//         xyz = 1;
+//     } else {
+//         btn.innerText = "ON"
+//         btn.style.color = "black"
+//         a.style.backgroundColor = "skyblue"
+//         xyz = 0;
+//     }
+// })
+
+
+var newBtn = document.createElement("button");
+newBtn.innerText = "Click me!";
+
+newBtn.style.textAlign = "center"
+document.querySelector("body").prepend(newBtn)
+
+var b = 0;
+newBtn.addEventListener("click", function () {
+    if (b == 0) {
+
+        newBtn.style.color = "white"
+        newBtn.style.backgroundColor = "black"
+        console.log("click hogia ");
+        b = 1;
     } else {
-        btn.innerText = "ON"
 
-        btn.style.backgroundColor = "red"
-        bulb.style.backgroundColor = "transparent"
-        console.log("Click the Again");
-        flag = 0;
+        newBtn.style.color = "blue"
+        newBtn.style.backgroundColor = "pink"
+        console.log('Again SnD ');
+        b = 0;
     }
-
 })
 
-
+// var newOk = document.querySelector("button");
+// newOk.setAttribute("class",".btn");
+// // // new1.setAttribute("newClass","btn")
+// newOk.setAttribute("class","btn")
